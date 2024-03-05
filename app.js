702bc0a7ea66c465dce78da7203af5610e7c6921
Anish Khari <anishkhari91@gmail.com>
2024-03-05 13:48:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const ytdlCore = require('ytdl-core')
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/download',async(req,res)=>{
+
+    try{
+        const url = req.body.url;
+        console.log(url)
+        const videoId = await ytdlCore.getURLVideoID(url)
+        const metaInfo = await ytdlCore.getInfo(url)
+        let data = {
+            url: 'https://www.youtube.com/embed/'+videoId,
+            info: metaInfo.formats
+        }
+        res.status(200).json(data);
+    }
+    catch(err){
+        res.status(500).json({error:err});
+    }
+    
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+})
